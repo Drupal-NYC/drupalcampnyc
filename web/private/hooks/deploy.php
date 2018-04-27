4e931d7d0cfd14bf9495b7ee769f904537c3247b
Shawn Duncan <sduncan@digitalpulp.com>
2018-04-27 15:05:41 -0400

    Move pantheon hooks in line with https://github.com/pantheon-systems/quicksilver-examples/#security

--- web/private/hooks/deploy.php
+++ web/private/hooks/deploy.php
@@ -1,20 +1,27 @@
 <?php
+
+/**
+ * @file
+ * Pantheon hooks.
+ */
+
 // Maintenance on.
 echo "Switch to maintenance mode...\n";
 passthru('drush sset system.maintenance_mode 1 -y');
-// Do-the-thing
-echo "thing\n";
-passthru('drush command');
-echo "command complete.\n";
+echo "In maintenance mode.\n";
+// Clear cache.
+echo "Clear all cache bins\n";
+passthru('drush cr');
+echo "Cache clear complete.\n";
 // Update database.
 echo "Apply database updates\n";
 passthru('drush updb -y');
 echo "Database updates complete.\n";
-// Config Import
+// Config Import.
 echo "Import configuration\n";
 passthru('drush cim sync -y');
 echo "Configuration import complete.\n";
-// Clear cache
+// Clear cache.
 echo "Clear all cache bins\n";
 passthru('drush cr');
 echo "Cache clear complete.\n";
