5b6fe5a8ec4d08e817e37704c765397721634cf7
JD Leonard <jd@modernbizconsulting.com>
2020-05-18 02:27:22 -0400

    Lots of configuration tweaks, branding tweaks

--- web/modules/custom/de_core/src/Plugin/SocialMediaLinks/Platform/DrupalSlack.php
+++ web/modules/custom/de_core/src/Plugin/SocialMediaLinks/Platform/DrupalSlack.php
@@ -5,12 +5,12 @@ namespace Drupal\de_core\Plugin\SocialMediaLinks\Platform;
 use Drupal\social_media_links\PlatformBase;
 
 /**
- * Provides 'medium' platform.
+ * Provides 'slack' platform.
  *
  * @Platform(
  *   id = "drupal_slack",
- *   name = @Translation("Drupal Slack"),
- *   urlPrefix = "https://drupal.slack.com/",
+ *   name = @Translation("DrupalNYC Slack"),
+ *   urlPrefix = "https://www.drupalnyc.org/",
  *   iconName = "slack",
  * )
  */
